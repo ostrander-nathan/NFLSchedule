f1ff8e901ca01a7baa16e245d7f67b2c48f718a0
Nathan <nathanostrander5@gmail.com>
2021-10-14 16:02:31 -0500

    add project

---
+++ NFLSchedule/NFLSchedule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NFLSchedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
